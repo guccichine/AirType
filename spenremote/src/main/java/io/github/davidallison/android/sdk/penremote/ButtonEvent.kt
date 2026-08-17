462148b03d7199b95f7b12d7e00ccd4f4bd7648d
guccichine <romanjames4020@gmail.com>
2026-08-18 07:25:09 +1000

    Add StrokeRecognizer and S Pen event classes

---
+++ spenremote/src/main/java/io/github/davidallison/android/sdk/penremote/ButtonEvent.kt
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2024 David Allison <davidallisongithub@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.davidallison.android.sdk.penremote
+
+class ButtonEvent(event: SPenEvent) {
+    val timeStamp: Long = event.timeStamp
+    val action: ButtonAction =
+        if (event.values[1].toInt() == 0) ButtonAction.ACTION_DOWN else ButtonAction.ACTION_UP
+
+    enum class ButtonAction(val code: Int) {
+        ACTION_DOWN(0), ACTION_UP(1),
+    }
+}
